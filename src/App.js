4c1b5a5d85c051a28b846e001ba33eb7e9e1b0a0
kelinn <36924700+kelinn@users.noreply.github.com>
2020-09-16 19:55:15 -0600

    contador

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 // Importamos nuestro componente
 import Hola from './Hola';
- 
+
 function App() {
   return (
     <div className="App">
--- src/Hola.js
+++ src/Hola.js
@@ -1,23 +1,17 @@
 
 import React, { Component } from 'react';
+import Contador from './Contador';
  
 class Hola extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          count: 0
-        };
-      }
   render() {
     return (
         <div>
         <h1>Hola mundo</h1>
         <h1>Kelin Silva</h1>
-        <p>Has dado click {this.state.count} veces</p>
-        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
-          Click me
-        </button>
+        <Contador/> 
       </div>
+   
+
       
     );
   }
